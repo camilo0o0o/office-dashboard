acc40fbf17fdf7c0eacf17de71ff6f4da85c95e6
Camilo Giraldo <camilo0o0o@users.noreply.github.com>
2025-04-04 13:55:56 +0100

    add bobby to homescreen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,15 @@
-import Image from "next/image";
 import { poppins } from "@/app/fonts";
+import Spline from '@splinetool/react-spline/next';
+
 
 export default function Home() {
   return (
-    <div className={`flex flex-col items-center justify-center h-screen ${poppins.className}`}>
-      <div className="flex flex-col items-center justify-center"></div>
+    <div className={`flex flex-col items-center justify-center h-full w-full`}>
       <h1 className="text-7xl font-bold p-5 text-center">🐝🛠️</h1>
-        <h1 className={`${poppins.className} text-7xl font-semibold p-5 text-center text-nlb`}>MUNCIȚI MUNCIȚI ALBINUȚELE MELE</h1>
-        <Image
-          src="/selfie_me.jpg"
-          alt="Selfie"
-          width={500}
-          height={300}
-          priority
-      />
+      <h1 className={`${poppins.className} text-7xl font-semibold p-5 text-center text-nlb`}>MUNCIȚI MUNCIȚI ALBINUȚELE MELE</h1>
+      <div className="w-fit h-fit">
+        <Spline scene="https://prod.spline.design/XUFsIrXg0YofwRdk/scene.splinecode" />
+      </div>
     </div>
   );
 }
